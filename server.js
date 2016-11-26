dd40c0606e3bc1d854ce81380ea0539cd773427c
sankhadipdas <ricky.das25@gmail.com>
2016-11-26 12:37:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,90 +1,24 @@
-<!DOCTYPE html>
-<html>
-<style>
-form {
-    border: 3px solid #f1f1f1;
-}
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-}
+var app = express();
+app.use(morgan('combined'));
 
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-}
 
-.cancelbtn {
-    width: auto;
-    padding: 10px 18px;
-    background-color: #f44336;
-}
+app.get('/',function (req,res) {
+    res.sendfile(path.join(__dirname, 'ui', 'index.html'));
+});
 
-.imgcontainer {
-    text-align: center;
-    margin: 24px 0 12px 0;
-}
+app.get('/ui/style.css',function (req,res) {
+    res.sendfile(path.join(__dirname, 'ui', 'style.css'));
+});
 
-img.avatar {
-    width: 40%;
-    border-radius: 50%;
-}
+app.get('/ui/madi.png',function (req,res) {
+    res.sendfile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
-.container {
-    padding: 16px;
-}
-
-span.psw {
-    float: right;
-    padding-top: 16px;
-}
-
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-    span.psw {
-       display: block;
-       float: none;
-    }
-    .cancelbtn {
-       width: 100%;
-    }
-}
-</style>
-<body>
-
-<h2>Login Form</h2>
-
-<form action="action_page.php">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
-
-  <div class="container">
-    <label><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
-
-    <label><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
-        
-    <button type="submit">Login</button>
-    <input type="checkbox" checked="checked"> Remember me
-  </div>
-
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
-</form>
-
-</body>
-</html>
+var port = 8080;
+app.listen(8080, function() {
+    console.log('IMAD course app is listening on port ${port}!');
+});
